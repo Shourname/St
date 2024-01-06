420c9e315966ff84ea8f83e0e985f4d437253c09
Shourname <145758777+Shourname@users.noreply.github.com>
2024-01-06 11:37:48 +1000

    Update StPack.cpp

--- StPack.cpp
+++ StPack.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <ctime>
 using namespace std;
+
+int rand_between(const int from, const int to) 
+{
+  if (to == from)
+    return to;
+  if (to < from)
+    return rand_between(to, from);
+  return from + rand() % (to-from+1);
+}
 
 class Student
 {
@@ -26,12 +36,39 @@ class Student
 
             if(s >= 4.6) return 1; else return 0;
         }
-        
 };
 
+class Teacher
+{
+    protected:
+        bool mood;
+        int changem;
+        string subject;
+    public:
+        Teacher(string NewSubject)
+        {
+            subject = NewSubject;
+        }
+        void setMood(bool NewMood) { mood = NewMood; }
+        virtual void UpgiveMarks(Student& st)
+        {
+            bool isSt = st.isExcellentStudent();
+            int rn45 = rand_between(4, 5);
+            int rn23 = rand_between(2, 3);
+
+            if(mood == true and isSt == true) st.giveMarks(5);
+            else if(mood == false and isSt == true) st.giveMarks(rn45);
+            else if(mood == true and isSt == false) st.giveMarks(4);
+            else if(mood == false and isSt == false) st.giveMarks(rn23);
+            changem++;
+            if(changem == 5) setMood(rand_between(0, 1));
+        }
+};
 
 int main()
 {
+    srand(unsigned(time(0)));
+    
     Student s1("Prohor", "Timofeev"), s2("Bordan", "Lazarev"), s3("Hohlova", "Haritina");
     s1.giveMarks(3);
     s1.giveMarks(4);
@@ -40,4 +77,7 @@ int main()
     s3.giveMarks(4);
     s3.giveMarks(2);
 
+    Teacher t1("Math"), t2("English");
+    t1.UpgiveMarks(s1);
+    t1.UpgiveMarks(s2);
 }
